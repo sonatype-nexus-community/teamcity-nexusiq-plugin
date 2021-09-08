ec0d9287a11576379dc5c3188fa853d08d92cf22
Hervé Boutemy <hboutemy@sonatype.com>
2021-09-08 13:05:19 +0200

    dont fail json parsing on unknown fields #19

--- teamcity-iq-plugin-server/src/main/java/org/sonatype/teamcity/server/tab/JsonParser.java
+++ teamcity-iq-plugin-server/src/main/java/org/sonatype/teamcity/server/tab/JsonParser.java
@@ -3,6 +3,7 @@ package org.sonatype.teamcity.server.tab;
 import org.sonatype.teamcity.results.IQScanResult;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -10,6 +11,7 @@ public class JsonParser {
     public static ObjectMapper getObjectMapper(){
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         return mapper;
     }
